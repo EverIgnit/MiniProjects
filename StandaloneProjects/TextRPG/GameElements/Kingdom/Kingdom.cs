8f3c56407b32efd3f9b81d2807367d7028d2b711
EverIgnit <gbelkevich@bk.ru>
2021-05-28 20:53:12 +0300

    Update Kingdom.cs

--- StandaloneProjects/TextRPG/GameElements/Kingdom/Kingdom.cs
+++ StandaloneProjects/TextRPG/GameElements/Kingdom/Kingdom.cs
@@ -1,6 +1,6 @@
-﻿using KingdomParameters;
-namespace TextRPG.GameElements.Kingdom
+﻿namespace TextRPG.GameElements.Kingdom
 {
+    using KingdomParameters;
     class Kingdom
     {
         public PopulationKSP MyProperty { get; set; }
